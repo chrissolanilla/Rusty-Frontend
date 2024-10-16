3b4ee5d0b4730e4a15d54ccdc61031efec14f1db
Christopher Solanilla <chrissolanilla@gmail.com>
2024-10-16 09:28:54 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<div class="container">
+	<h1>Welcome to the Rusty Reef</h1>
+	<div class="imageContainer">
+		<img src="crab-rave.gif" alt="crab rave gif" class="gif" >
+	</div>
+	<h2>Lets test how rusty you are</h2>
+	<button>Join now</button>
+</div>
+
+<style lang="scss">
+	:global(body) {
+		margin: 0;
+				background-image: url('rustyReef.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+
+		}
+	* {
+		text-align: center;
+		color: red;
+	}
+	.container {
+		background-image: url('rustyReef.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		.imageContainer {
+			display: flex;
+			justify-content: center;
+		}
+	}
+
+	.gif {
+		width:30%;
+		object-fit:cover;
+		object-position: 0 -150px;
+		height: auto;
+	}
+</style>
